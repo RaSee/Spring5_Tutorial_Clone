784b88f4134ab9f6d94dabff8f7d98b42e689241
RaSee <tkwpvk789@gmail.com>
2022-01-07 02:16:59 +0900

    [Chapter 04]
    - Add required parameter
    
    etc
    1. Autowired(required = false)
    2. @Nullable annotation
    3. Optional<class> class

--- sp5-chap03/src/main/java/chap03/MemberPrinter.java
+++ sp5-chap03/src/main/java/chap03/MemberPrinter.java
@@ -1,11 +1,25 @@
 package chap03;
 
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class MemberPrinter {
 	
+	
+	private DateTimeFormatter dateTimeFormatter;
+	
 	public void print(Member member) {
+		
 		System.out.printf(
 				"회원 정보: 아이디 = %d, 이메일 = %s, 이름 = %s, 등록일 = %tF\n",
-				member.getID(),member.getEmail(), member.getName(), member.getRegisterDateTime());
+				member.getID(),member.getEmail(), member.getName(), 
+				dateTimeFormatter == null ? member.getRegisterDateTime() : dateTimeFormatter.format(member.getRegisterDateTime()));
+	}
+	
+	@Autowired(required = false)
+	public void setDateFormatter(DateTimeFormatter dateTimeFormatter) {
+		this.dateTimeFormatter = dateTimeFormatter;
 	}
 
 }
